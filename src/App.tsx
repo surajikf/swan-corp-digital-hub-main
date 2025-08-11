df0fe263996af3ee18b72967ec9615097c068a3e
Suraj S <surajikf@gmail.com>
2025-08-11 18:25:37 +0530

    Fix routing for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import CustomCursor from "@/components/ui/custom-cursor";
 import BackToTop from "@/components/ui/back-to-top";
 import Index from "./pages/Index";
@@ -25,7 +25,7 @@ const App = () => (
       <CustomCursor>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
+        <HashRouter>
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="/about" element={<About />} />
@@ -52,7 +52,7 @@ const App = () => (
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
             <Route path="*" element={<NotFound />} />
           </Routes>
-        </BrowserRouter>
+        </HashRouter>
         <BackToTop />
       </CustomCursor>
     </TooltipProvider>
